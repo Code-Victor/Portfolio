12dc960d317312b3f7a21335c2f1ae90e04f5b90
Code-Victor <oluwaborihamzat@gmail.com>
2022-07-26 14:34:49 +0100

    added my design tokens

---
+++ stitches.config.ts
@@ -0,0 +1,270 @@
+import { createStitches } from "@stitches/react";
+import type * as Stitches from "@stitches/react";
+export type { VariantProps } from "@stitches/react";
+
+export const {
+  styled,
+  css,
+  theme,
+  createTheme,
+  getCssText,
+  globalCss,
+  keyframes,
+  config,
+  reset,
+} = createStitches({
+  theme: {
+    colors: {
+      textPrimary: "#F0F0F0",
+      textSecondary: "#C4C4C4",
+      backgroundPrimary: "#111111",
+      backgroundSecondary: "#000000",
+      // Semantic colors
+      hiContrast: "$slate12",
+      // loContrast: '$slate1',
+      loContrast: "white",
+      canvas: "hsl(0 0% 93%)",
+      panel: "white",
+      transparentPanel: "hsl(0 0% 0% / 97%)",
+      shadowLight: "hsl(206 22% 7% / 35%)",
+      shadowDark: "hsl(206 22% 7% / 20%)",
+    },
+    fonts: {
+      untitled: "Untitled Sans, -apple-system, system-ui, sans-serif",
+      mono: "Söhne Mono, menlo, monospace",
+      inter: "Inter, sans-serif",
+      poppins: "Poppins, sans-serif",
+      ogg: "Ogg, sans-serif",
+    },
+    space: {
+      1: "5px",
+      2: "10px",
+      3: "15px",
+      4: "20px",
+      5: "25px",
+      6: "35px",
+      7: "45px",
+      8: "65px",
+      9: "80px",
+    },
+    fontWeights: {
+      1: "300",
+      2: "400",
+      3: "500",
+      4: "600",
+      5: "700",
+    },
+    sizes: {
+      1: "5px",
+      2: "10px",
+      3: "15px",
+      4: "20px",
+      5: "25px",
+      6: "35px",
+      7: "45px",
+      8: "65px",
+      9: "80px",
+    },
+    fontSizes: {
+      1: "12px",
+      2: "13px",
+      3: "15px",
+      4: "16px",
+      5: "20px",
+      6: "24px",
+      7: "28px",
+      8: "36px",
+      9: "60px",
+      10: "78px",
+    },
+    radii: {
+      1: "4px",
+      2: "6px",
+      3: "8px",
+      4: "12px",
+      round: "50%",
+      pill: "9999px",
+    },
+    zIndices: {
+      1: "100",
+      2: "200",
+      3: "300",
+      4: "400",
+      max: "999",
+    },
+  },
+  media: {
+    sm: "(min-width: 640px)",
+    md: "(min-width: 768px)",
+    lg: "(min-width: 1024px)",
+    xl: "(min-width: 1280px)",
+    xxl: "(min-width: 1536px)",
+    motion: "(prefers-reduced-motion)",
+    hover: "(any-hover: hover)",
+    dark: "(prefers-color-scheme: dark)",
+    light: "(prefers-color-scheme: light)",
+  },
+  utils: {
+    p: (value: Stitches.PropertyValue<"padding">) => ({
+      padding: value,
+    }),
+    pt: (value: Stitches.PropertyValue<"paddingTop">) => ({
+      paddingTop: value,
+    }),
+    pr: (value: Stitches.PropertyValue<"paddingRight">) => ({
+      paddingRight: value,
+    }),
+    pb: (value: Stitches.PropertyValue<"paddingBottom">) => ({
+      paddingBottom: value,
+    }),
+    pl: (value: Stitches.PropertyValue<"paddingLeft">) => ({
+      paddingLeft: value,
+    }),
+    px: (value: Stitches.PropertyValue<"paddingLeft">) => ({
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+    py: (value: Stitches.PropertyValue<"paddingTop">) => ({
+      paddingTop: value,
+      paddingBottom: value,
+    }),
+
+    m: (value: Stitches.PropertyValue<"margin">) => ({
+      margin: value,
+    }),
+    mt: (value: Stitches.PropertyValue<"marginTop">) => ({
+      marginTop: value,
+    }),
+    mr: (value: Stitches.PropertyValue<"marginRight">) => ({
+      marginRight: value,
+    }),
+    mb: (value: Stitches.PropertyValue<"marginBottom">) => ({
+      marginBottom: value,
+    }),
+    ml: (value: Stitches.PropertyValue<"marginLeft">) => ({
+      marginLeft: value,
+    }),
+    mx: (value: Stitches.PropertyValue<"marginLeft">) => ({
+      marginLeft: value,
+      marginRight: value,
+    }),
+    my: (value: Stitches.PropertyValue<"marginTop">) => ({
+      marginTop: value,
+      marginBottom: value,
+    }),
+
+    ta: (value: Stitches.PropertyValue<"textAlign">) => ({ textAlign: value }),
+
+    fd: (value: Stitches.PropertyValue<"flexDirection">) => ({
+      flexDirection: value,
+    }),
+    fw: (value: Stitches.PropertyValue<"flexWrap">) => ({ flexWrap: value }),
+
+    ai: (value: Stitches.PropertyValue<"alignItems">) => ({
+      alignItems: value,
+    }),
+    ac: (value: Stitches.PropertyValue<"alignContent">) => ({
+      alignContent: value,
+    }),
+    jc: (value: Stitches.PropertyValue<"justifyContent">) => ({
+      justifyContent: value,
+    }),
+    as: (value: Stitches.PropertyValue<"alignSelf">) => ({ alignSelf: value }),
+    fg: (value: Stitches.PropertyValue<"flexGrow">) => ({ flexGrow: value }),
+    fs: (value: Stitches.PropertyValue<"flexShrink">) => ({
+      flexShrink: value,
+    }),
+    fb: (value: Stitches.PropertyValue<"flexBasis">) => ({ flexBasis: value }),
+    gcs: (value: Stitches.PropertyValue<"gridColumn">) => ({
+      gridColumn: value,
+    }),
+    grs: (value: Stitches.PropertyValue<"gridRow">) => ({ gridRow: value }),
+    bg: (value: Stitches.PropertyValue<"background">) => ({
+      background: value,
+    }),
+    bc: (value: Stitches.PropertyValue<"backgroundColor">) => ({
+      backgroundColor: value,
+    }),
+
+    br: (value: Stitches.PropertyValue<"borderRadius">) => ({
+      borderRadius: value,
+    }),
+    btrr: (value: Stitches.PropertyValue<"borderTopRightRadius">) => ({
+      borderTopRightRadius: value,
+    }),
+    bbrr: (value: Stitches.PropertyValue<"borderBottomRightRadius">) => ({
+      borderBottomRightRadius: value,
+    }),
+    bblr: (value: Stitches.PropertyValue<"borderBottomLeftRadius">) => ({
+      borderBottomLeftRadius: value,
+    }),
+    btlr: (value: Stitches.PropertyValue<"borderTopLeftRadius">) => ({
+      borderTopLeftRadius: value,
+    }),
+
+    bs: (value: Stitches.PropertyValue<"boxShadow">) => ({ boxShadow: value }),
+
+    lh: (value: Stitches.PropertyValue<"lineHeight">) => ({
+      lineHeight: value,
+    }),
+
+    ox: (value: Stitches.PropertyValue<"overflowX">) => ({ overflowX: value }),
+    oy: (value: Stitches.PropertyValue<"overflowY">) => ({ overflowY: value }),
+
+    pe: (value: Stitches.PropertyValue<"pointerEvents">) => ({
+      pointerEvents: value,
+    }),
+    us: (value: Stitches.PropertyValue<"userSelect">) => ({
+      WebkitUserSelect: value,
+      userSelect: value,
+    }),
+    mw:(value:Stitches.PropertyValue<"maxWidth">)=>({maxWidth:value}),
+    userSelect: (value: Stitches.PropertyValue<"userSelect">) => ({
+      WebkitUserSelect: value,
+      userSelect: value,
+    }),
+
+    size: (value: Stitches.PropertyValue<"width">) => ({
+      width: value,
+      height: value,
+    }),
+    
+
+    appearance: (value: Stitches.PropertyValue<"appearance">) => ({
+      WebkitAppearance: value,
+      appearance: value,
+    }),
+    backgroundClip: (value: Stitches.PropertyValue<"backgroundClip">) => ({
+      WebkitBackgroundClip: value,
+      backgroundClip: value,
+    }),
+  },
+});
+
+export const darkTheme = createTheme({
+  colors: {
+    text: "white",
+    background: "black",
+  },
+});
+export type CSS = Stitches.CSS<typeof config>;
+
+export const GlobalStyles = globalCss({
+  "@import": [
+    "url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@400;500&display=swap')",
+  ],
+  "@font-face": [
+    {
+      fontFamily: "Ogg",
+      src: "url('https://d33wubrfki0l68.cloudfront.net/db8b8782c3fa4ec2f05eb5361e44d02176b855ce/3424a/fonts/ogg-regular.woff2')",
+    },
+  ],
+  "*,*::before,*::after": {
+    boxSizing: "border-box",
+    margin: 0,
+    padding: 0,
+    fontFamily: "$inter",
+  },
+});
+
+GlobalStyles();
