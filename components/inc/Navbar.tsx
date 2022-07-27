a03918d73738d7ee2c4e411bce803eda3547728e
Code-Victor <oluwaborihamzat@gmail.com>
2022-07-27 23:28:40 +0100

    added theme controller

--- components/inc/Navbar.tsx
+++ components/inc/Navbar.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { Box,Flex, Text,Container } from "../base";
-import { Sun } from "../icons";
+import { Sun,Moon } from "../icons";
+import { useTheme } from "next-themes";
 
 
 const Navbar = () => {
+  const { theme, setTheme } = useTheme();
   return (
-    <Box css={{bg: "$backgroundPrimary" }}>
+    <Box css={{ bg: "$backgroundPrimary", color: "$textPrimary" }}>
       <Container>
         <Flex
           justify={"between"}
           align={"center"}
-          css={{ height:'70px', bg: "$backgroundPrimary" }}
+          css={{ height: "70px", bg: "$backgroundPrimary" }}
         >
-          <Text as="h1" fontFamily={"inter"} fontSize={6}>
+          <Text
+            as="h1"
+            css={{ color: "$textPrimary" }}
+            fontFamily={"inter"}
+            fontSize={6}
+          >
             HV.
           </Text>
           <Flex gap={5} align="center">
@@ -25,7 +32,16 @@ const Navbar = () => {
             <Text as="a" href="#">
               Contact
             </Text>
-            <Sun />
+            <button
+              style={{
+                border: "none",
+                outline: "none",
+                background: "transparent",
+              }}
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            >
+              {theme === "dark" ? <Sun /> : <Moon/>}
+            </button>
           </Flex>
         </Flex>
       </Container>
