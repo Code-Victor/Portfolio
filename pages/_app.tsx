5d4ff9c0f38bc9d3259d21f10cf0502a9a7cf619
Code-Victor <oluwaborihamzat@gmail.com>
2022-07-27 23:32:03 +0100

    added themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { darkTheme } from "../stitches.config";
+import { ThemeProvider } from "next-themes";
+import { Navbar } from '../components/inc';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+        <ThemeProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem={false}
+      value={{
+        light: "light",
+        dark: darkTheme.className,
+      }}
+    >
+      <Navbar/>
+    <Component {...pageProps} />
+    </ThemeProvider>)
 }
 
 export default MyApp
