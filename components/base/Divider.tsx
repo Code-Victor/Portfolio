29fe3c474080afda49cd8f90f248ac24b9c7c809
Code-Victor <oluwaborihamzat@gmail.com>
2022-07-27 23:23:50 +0100

    created Divider base componet

---
+++ components/base/Divider.tsx
@@ -0,0 +1,9 @@
+import { styled } from "../../stitches.config";
+
+const Divider = styled("div", {
+    width:'100%',
+    height:0.6,
+    backgroundColor:'$textSecondary',
+});
+
+export default Divider;
