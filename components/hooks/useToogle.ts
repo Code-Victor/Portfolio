7f3a6d086b428863f78387cd519ab7e24fd50f31
Code-Victor <oluwaborihamzat@gmail.com>
2022-07-29 15:56:08 +0100

    got a useToggle hook from 'rooks'

--- components/hooks/useToogle.ts
+++ components/hooks/useToogle.ts
@@ -1,9 +1,17 @@
-import { useCallback, useState } from "react";
+import { useReducer } from "react";
 
-export default function useToggle(initialValue = false) {
-  const [value, setValue] = useState(initialValue);
-  const toggle = useCallback(() => {
-    setValue((v) => !v);
-  }, []);
-  return [value, toggle];
+const defaultToggleFunction = (v: boolean): boolean => !v;
+
+/**
+ * Use toggle hook helps you easily toggle a value
+ *
+ * @param initialValue Initial value of the toggle
+ * @param toggleFunction A toggle function. This allows for non boolean toggles
+ */
+function useToggle(
+  initialValue: any = false,
+  toggleFunction: (state: any, action: any) => any = defaultToggleFunction
+): [any, (action: any) => any] {
+  return useReducer(toggleFunction, initialValue);
 }
+export default useToggle
