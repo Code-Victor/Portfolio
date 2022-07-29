bf2d4c707da6251bb7810e4884632132884994b7
Code-Victor <oluwaborihamzat@gmail.com>
2022-07-29 15:59:23 +0100

    added id prop

--- components/inc/Section.tsx
+++ components/inc/Section.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { CSS } from "../../stitches.config";
 import { Box, Container, Text } from "../base";
-type sectionProps = { title: string; children: React.ReactNode; css: CSS }; 
-const Section = ({ title, children, css }: sectionProps) => {
+type sectionProps = { title: string; children: React.ReactNode; css: CSS,id?:string }; 
+const Section = ({ title, children, css ,id}: sectionProps) => {
   return (
-    <Box css={{pt:'$6',...css}}>
+    <Box css={{pt:'$6',...css}} id={id?id:undefined}>
       <Container>
         <Text as='h3'fontFamily={'poppins'}  fontWeight="medium" css={{mb:'$2','@md':{mb:'$4'}}} >{title}</Text>
       {children}
