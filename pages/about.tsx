322fc8f6deb60bd2c1f9b0da44404f1e37fc5aca
Code-Victor <oluwaborihamzat@gmail.com>
2022-08-03 19:58:33 +0100

    started the about page

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Experience, Intro, Journey, Tools } from '../components/inc'
+
+const about = () => {
+  return (
+    <>
+    <Intro/>
+    <Journey/>
+    <Experience/>
+    <Tools/>
+    </>
+  )
+}
+
+export default about
