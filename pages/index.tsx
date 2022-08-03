ff836e6e698cf7e9746e281ba9954f6c3e2f40c6
Code-Victor <oluwaborihamzat@gmail.com>
2022-08-03 19:58:58 +0100

    changed layout and added server side code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,34 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import { Container } from "../components/base";
-import { Experience,Blog, Featured, Hero, NewsLetter, Tools } from "../components/inc";
+import { SWRConfig } from "swr";
+import { Experience,Blog, Featured, Hero, NewsLetter, Tools, DDoM } from "../components/inc";
+import { getArticles, returns } from "../components/utils/";
 
-const Home: NextPage = () => {
+const Home: NextPage<{fallback:returns}> = ({fallback}) => {
   return (
+     <SWRConfig value={{ fallback }}>
+
     <>
       <Hero/>
-      <Experience/>
-      <Tools/>
-      <Featured/>
       <Blog/>
-      {/* TODO: Add Featured articles */}
+      <Featured/>
+      <DDoM/>
+      {/* TODO: Add Daily dose of motivation*/}
       <NewsLetter/>
     </>
+     </SWRConfig>
   );
 };
 
+ export async function getStaticProps() {
+   // `getStaticProps` is executed on the server side.
+   const article = await getArticles<returns>();
+   return {
+     props: {
+       fallback: {
+         "/api/article": article,
+       },
+     },
+   };
+ }
 export default Home;
