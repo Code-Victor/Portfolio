d87b62a20ce09f39939b844804488e8dc9e612c9
Code-Victor <oluwaborihamzat@gmail.com>
2022-08-04 03:15:32 +0100

    prevents overflow during exit animaitons

--- components/inc/Experience.tsx
+++ components/inc/Experience.tsx
@@ -26,7 +26,7 @@ const TabHeader = styled("div", {
 
 const Experience = () => {
   return (
-    <Section id="experience" title="Experience" css={{ bg: "$backgroundSecondary" }}>
+    <Section id="experience" title="Experience" css={{ bg: "$backgroundSecondary",overflow:'hidden' }}>
       <Tabs />
     </Section>
   );
