f4456ecd3699067847b7354ec5ce7e2a36cd89f1
Code-Victor <oluwaborihamzat@gmail.com>
2022-08-11 13:37:13 +0100

    added more accessible aria-label ,moved hamburger

--- src/components/inc/Navbar.tsx
+++ src/components/inc/Navbar.tsx
@@ -1,29 +1,17 @@
-import React, { useRef, useEffect, useState, useContext } from "react";
-import { Box, Flex, Text, Container, NiceLink } from "../base";
+import React, { useEffect, useState, useContext } from "react";
+import { Box, Flex, Container, NiceLink } from "../base";
 import { Sun, Moon, Logo } from "../icons";
 import { useTheme } from "next-themes";
 import config from "@config";
 import { motion } from "framer-motion";
 import { LoaderContext } from "@context";
-import { styled, keyframes } from "@stitchesConfig";
-import useToggle from "@hooks/useToogle";
 
 const { navLinks } = config;
-
-const collapse = keyframes({
-  "0%": {
-    transform: "translateY(-100%)",
-  },
-  "50%": {
-    transform: "translateY(-100%) rotate(45deg)",
-  },
-});
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
   const [top, setTop] = useState(false);
   const { loading } = useContext(LoaderContext);
-  const [navOpen, setNavOpen] = useToggle(false);
 
   const topObserver = () => {
     setTop(window.scrollY > 0);
@@ -62,12 +50,6 @@ const Navbar = () => {
               <Logo />
             </motion.div>
           }
-          <Hamburger active={navOpen} onClick={setNavOpen}>
-            <Box />
-            <Box />
-            <Box />
-            <Box />
-          </Hamburger>
           <Flex
             gap={5}
             align="center"
@@ -94,7 +76,9 @@ const Navbar = () => {
               </NiceLink>
             ))}
             <button
-              aria-label="Toggle Theme"
+              aria-label={`${
+                theme === "dark" ? "set theme to light" : "set theme to dark"
+              }`}
               style={{
                 border: "none",
                 outline: "none",
@@ -111,71 +95,4 @@ const Navbar = () => {
   );
 };
 
-const Hamburger = styled("button", {
-  position: "relative",
-  border: "none",
-  opacity: 0.75,
-  outline: "none",
-  width: "45px",
-  gap: 10,
-  display: "flex",
-  bg: "transparent",
-  jc: "space-between",
-  fd: "column",
-  transition: "all 0.3s ease-in-out",
-  "& > div": {
-    height: 3,
-    bg: "black",
-    br: "$3",
-    transition: "all 0.3s ease-in-out",
-  },
-  "& > div:nth-child(1)": {
-    width: "50%",
-    transformOrigin: "left center",
-  },
-  "& > div:nth-child(2)": {
-    width: "100%",
-    transformOrigin: "center",
-  },
-  "& > div:nth-child(3)": {
-    transformOrigin: "right center",
-    width: "50%",
-    alignSelf: "flex-end",
-  },
-  "& > div:nth-child(4)": {
-    position: "absolute",
-    transformOrigin: "center",
-    width: "100%",
-    top: "50%",
-    transform: "translateY(-50%)",
-  },
-  "@md": {
-    display: "none",
-  },
-  variants: {
-    active: {
-      true: {
-        gap: 0,
-        "& > div:nth-child(1)": {
-          transform: "translateY(100%)",
-          opacity: 0,
-          brr: 0,
-        },
-        "& > div:nth-child(2)": {
-          transform: "rotate(-45deg)",
-          transitionDelay: "0.3s",
-        },
-        "& > div:nth-child(3)": {
-          transform: "translateY(-100%)",
-          opacity: 0,
-          blr: 0,
-        },
-        "& > div:nth-child(4)": {
-          transform: "translateY(-50%) rotate(45deg)",
-          transitionDelay: "0.3s",
-        },
-      },
-    },
-  },
-});
 export default Navbar;
