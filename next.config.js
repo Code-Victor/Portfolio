ec29f6a9184ef35ea4b1797b3d12a1ac025781de
Code-Victor <oluwaborihamzat@gmail.com>
2022-08-13 12:06:37 +0100

    added medium as image domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["cdn-images-1.medium.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
