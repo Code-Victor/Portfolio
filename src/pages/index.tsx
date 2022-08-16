afec1097631e59275a137d8bea1bfb9b0f21f482
Code-Victor <oluwaborihamzat@gmail.com>
2022-08-16 01:24:42 +0100

    resolve suspense individual

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,33 @@
 import type { NextPage } from "next";
 import React, { Suspense } from "react";
-import Head from "next/head";
 import { SWRConfig } from "swr";
 import { Blog, Hero, LoadingScreen } from "@components/inc";
 import { getArticles, returns } from "@utils";
 import { LoaderContext } from "@context";
 import dynamic from "next/dynamic";
 
-const Featured = dynamic(() => import("@components/inc/Featured"));
-const DDoM = dynamic(() => import("@components/inc/DDoM"));
-const NewsLetter = dynamic(() => import("@components/inc/NewsLetter"));
+const Featured = dynamic(() => import("@components/inc/Featured"), {
+  suspense: true,
+});
+const DDoM = dynamic(() => import("@components/inc/DDoM"), { suspense: true });
+const NewsLetter = dynamic(() => import("@components/inc/NewsLetter"), {
+  suspense: true,
+});
 
 const Home: NextPage<{ fallback: returns }> = ({ fallback }) => {
   const Loader = React.useContext(LoaderContext);
   return (
     <SWRConfig value={{ fallback }}>
-      <Head>
-        <title>Hamzat Victor | Fronted Developer, Writter</title>
-        <meta
-          name="description"
-          content="Passionate Front-end engineer with react. Change the world one line of code at a time. Enjoy building accessible web interfaces while not leaving out visual consistency with the help of design systems."
-        />
-      </Head>
       <Hero />
       {Loader?.loading && <LoadingScreen />}
       <Blog />
       <Suspense fallback={<div />}>
         <Featured />
+      </Suspense>
+      <Suspense fallback={<div />}>
         <DDoM />
+      </Suspense>
+      <Suspense fallback={<div />}>
         <NewsLetter />
       </Suspense>
     </SWRConfig>
