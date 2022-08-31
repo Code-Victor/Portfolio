03454ea3f5331f52533da2bb441d1cfd48935414
Code-Victor <oluwaborihamzat@gmail.com>
2022-08-31 09:45:13 +0100

    added api routes for mdx

---
+++ src/pages/api/index.ts
@@ -0,0 +1,50 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
+import fs from "fs";
+import { sync } from "glob";
+import matter from "gray-matter";
+
+const SC_PATH = path.join(process.cwd(), "src", "static", "showcase");
+
+export const getSCSlug = (): string[] => {
+  const paths = sync("*.mdx", { cwd: SC_PATH });
+  return paths.map((path) => {
+    const [slug, _] = path.split(".");
+    return slug;
+  });
+};
+
+export const getAllSC = () => {
+  const showcases = getSCSlug().map((slug) => getSC(slug));
+  console.log(getSCSlug());
+};
+
+interface Post {
+  content: string;
+  meta: PostMeta;
+}
+export interface PostMeta {
+  title: string;
+  slug: string;
+  excerpt: string;
+  date: string;
+  tags: string[];
+}
+
+export const getSC = (slug: string): Post => {
+  const postPath = path.join(SC_PATH, `${slug}.mdx`);
+  const source = fs.readFileSync(postPath);
+  const { content, data } = matter(source);
+
+  return {
+    content,
+    meta: {
+      slug,
+      title: data.title ?? "",
+      excerpt: data.excerpt ?? slug,
+      date: data.date ?? new Date().toString(),
+      tags: (data.tags ?? []).sort(),
+    },
+  };
+};
