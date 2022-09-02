6b4005daba5756212fd74905144bb198d5ad2334
Code-Victor <oluwaborihamzat@gmail.com>
2022-09-02 14:26:09 +0100

    added framer motion chevron Icon

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -421,3 +421,55 @@ export const Firebase = (props: svgProps) => {
     </svg>
   );
 };
+
+export const FramerMotion = (props: svgProps) => {
+  return (
+    <svg
+      width="100"
+      height="100"
+      viewBox="0 0 100 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <circle cx="50" cy="50" r="50" fill="url(#paint0_linear_222_8)" />
+      <path
+        d="M25 75V25L50 50L75 25V75L62.5 62.5L50 75L37.5 62.5L25 75Z"
+        fill="white"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_222_8"
+          x1="80"
+          y1="17"
+          x2="26.5"
+          y2="86.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#D93E83" />
+          <stop offset="1" stopColor="#A15FDD" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export const ChevronRight=(props:svgProps)=>{
+  return (
+    <svg
+      width="15"
+      height="15"
+      viewBox="0 0 15 15"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M6.1584 3.13508C6.35985 2.94621 6.67627 2.95642 6.86514 3.15788L10.6151 7.15788C10.7954 7.3502 10.7954 7.64949 10.6151 7.84182L6.86514 11.8418C6.67627 12.0433 6.35985 12.0535 6.1584 11.8646C5.95694 11.6757 5.94673 11.3593 6.1356 11.1579L9.565 7.49985L6.1356 3.84182C5.94673 3.64036 5.95694 3.32394 6.1584 3.13508Z"
+        fill="currentColor"
+        fillRule="evenodd"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
